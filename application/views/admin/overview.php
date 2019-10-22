c93986961002c90239662123e2a3cc8a0cab4749
Muyinda Rogers <moverr@gmail.com>
2019-10-22 20:37:58 +0300

    :Debugging

---
+++ application/views/admin/overview.php
@@ -0,0 +1,35 @@
+<div class="container-fluid">
+
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+  <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+</div>
+
+<div class="row">
+
+   
+
+
+
+<div class="row">
+
+  <div class="col-lg-12">
+
+    <!-- Default Card Example -->
+    <div class="card mb-12">
+      <div class="card-header">
+      Dashboard 
+      </div>
+      <div class="card-body">
+       
+       Welcome to the dashboard, use the links on the side to navigate through the dashboard 
+    
+      </div>
+    </div>
+
+  </div>
+
+   
+</div>
+
+</div>
