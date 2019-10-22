1897af0042dfde748b5becaa70bd3d7d5ac559b9
Muyinda Rogers <moverr@gmail.com>
2019-10-22 19:31:01 +0300

    :Debugging

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -22,7 +22,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 </head>
 
-<body class="bg-gradient-primary">
+<body >
 
   <div class="container">
 
@@ -58,20 +58,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                       Login
                     </a>
                     <hr>
-                    <a href="index.html" class="btn btn-google btn-user btn-block">
-                      <i class="fab fa-google fa-fw"></i> Login with Google
-                    </a>
-                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                    </a>
+                    
+                    
                   </form>
-                  <hr>
-                  <div class="text-center">
-                    <a class="small" href="forgot-password.html">Forgot Password?</a>
-                  </div>
-                  <div class="text-center">
-                    <a class="small" href="register.html">Create an Account!</a>
-                  </div>
+                  
                 </div>
               </div>
             </div>
