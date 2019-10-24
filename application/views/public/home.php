3b35fdea5b8b49eb7998b45223c96799f81a5d22
Muyinda Rogers <moverr@gmail.com>
2019-10-24 13:42:40 +0300

    :Debuging

--- application/views/public/home.php
+++ application/views/public/home.php
@@ -327,6 +327,21 @@ Interested parties must get in touch with the company through our phone numbers 
 </footer>
     </div>
 
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5db17a80df22d91339a0d2a4/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+
 <script src="./js/jquery.min.js"></script>
 <script src="./js/app.js"></script>
 <script src="./js/jquery.waypoints.min.js"></script>
