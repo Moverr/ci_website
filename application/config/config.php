1f43607e7fdb1490aaa35e7b403e3b3ca8b381c3
Muyinda Rogers <moverr@gmail.com>
2019-11-05 13:47:42 +0300

    :Debugging

--- application/config/config.php
+++ application/config/config.php
@@ -23,51 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = '';
-
-define('SECURE_MODE', FALSE);
-
-	define('BASE_URL', 'http://localhost/tmis/');#Set to HTTPS:// if SECURE_MODE = TRUE
-
-	define('RETRIEVE_URL_DATA_IGNORE', 3);#The starting point to obtain the passed url data
-
-	define('SITE_TITLE', "TMIS");
-
-	define('SITE_SLOGAN', "");
-
-	define('SYS_TIMEZONE', "Africa/Nairobi");
-
-	define('NUM_OF_ROWS_PER_PAGE', "5");
-
-	define('NUM_OF_LISTS_PER_VIEW', "10");
-
-	define('IMAGE_URL', BASE_URL."assets/images/");
-
-	define('HOME_URL', getcwd()."/");
-
-	define('DEFAULT_CONTROLLER', 'page');
-
-	define('UPLOAD_DIRECTORY',  HOME_URL."assets/uploads/");
-
-	define('MAX_FILE_SIZE', 40000000);
-
-	define('ALLOWED_EXTENSIONS', ".doc,.docx,.txt,.pdf,.xls,.xlsx,.jpeg,.png,.jpg,.gif");
-
-	define('MAXIMUM_FILE_NAME_LENGTH', 100);
-
-	define("MINIFY", FALSE);
-
-	define('PORT_HTTP', '80');
-
-  	define('PORT_HTTP_SSL', '443');
-
-	define('PHP_LOCATION', "php5");
-
- 	define('ENABLE_PROFILER', FALSE); #See perfomance stats based on set benchmarks
-
-	define('DOWNLOAD_LIMIT', 10000); #Max number of rows that can be downloaded
-
-    define('RETIREMENT_AGE', 60); # Mandatory retirement age
+$config['base_url'] = 'http://localhost:8082/website/';
+ 
     
 
 /*
