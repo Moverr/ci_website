c4f35d24e21a37d51fc2e3a9d226f00b7abc96d7
Muyinda Rogers <moverr@gmail.com>
2019-11-05 19:00:20 +0300

    Wonderful :

--- application/models/DashboardModel.php
+++ application/models/DashboardModel.php
@@ -17,9 +17,31 @@ class DashboardModel extends CI_Model
 		return $this->_query_reader->get_row_as_array('get_page_by_name', array('page'=>$page )); 
 	 }
 	 
-	 function savePage($details){
+	  
+	 function save($schoolDetails)
+	 {
+		 #print_r($schoolDetails); exit();
+		 $isAdded = false;
+		 $required = array('schoolname', 'schooltype__schooltypes', 'dateschoolregistered', 'schooltelephone', 'schooladdress__addressline');
 		 
+		 # 1. Add all provided data into the session
+		 $passed = process_fields($this, $schoolDetails, $required, array("-"));
+		 $msg = !empty($passed['msg'])? $passed['msg']: "";
+		 # 2. Save the data into the database
+		 if($passed['boolean'])
+		 {
+		
+			$emailContactId = $this->_query_reader->add_data('add_contact_data', array('contact_type'=>'email', 'carrier_id'=>'', 'details'=>$details['schoolemailaddress'], 'parent_id'=>$schoolId, 'parent_type'=>'school'));
+			
+		 }
+		 
+		 return array('boolean'=>$isAdded, 'msg'=>$msg));
+
 	 }
+	 
+	 
+
+	 
 
 	
 	
